ff87bccd6a9ff28265f6fcb8dd50b437fbae1029
HoaAnDuong <an.duong@makeai.vn>
2025-06-04 22:07:14 +0700

    feat: Add Face Recognition Setting and Collection doctypes

--- face_recognition/face_recognition/doctype/collection/collection.js
+++ face_recognition/face_recognition/doctype/collection/collection.js
@@ -98,6 +98,7 @@ frappe.ui.form.on('Collection', {
                                         const faceListDiv = document.getElementById('recognized_faces_list');
 
                                         img.onload = () => {
+                                            frappe.msgprint("img.onload");
                                             // Set canvas dimensions to match the image's rendered dimensions
                                             canvas.width = img.clientWidth;
                                             canvas.height = img.clientHeight;
