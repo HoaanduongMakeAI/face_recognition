d881735d1e2eebe537ed3904383bbee2b46c097a
HoaAnDuong <an.duong@makeai.vn>
2025-06-04 10:45:09 +0700

    feat: Add Face Recognition Setting and Collection doctypes

--- face_recognition/face_recognition/doctype/collection/collection.py
+++ face_recognition/face_recognition/doctype/collection/collection.py
@@ -17,8 +17,6 @@ class Collection(Document):
         endpoint = f"{server_url}/list_faces/{collection_name}"
         headers = {"X-API-Key": api_key}
 
-        frappe.throw(f"{api_key}")
-
         try:
             response = requests.get(endpoint, headers=headers)
             response.raise_for_status() # Raise HTTPError for bad responses (4xx or 5xx)
